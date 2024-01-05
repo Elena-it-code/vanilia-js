cf720a69dc24661efaa85454d6937355eccc2f4f
ElenaKuklina <cuklina.lena@gmail.com>
2024-01-05 22:36:51 +0300

    Lesson_09: "array test". using the push() method as an example

--- src/js_09_01/09_01.test.ts
+++ src/js_09_01/09_01.test.ts
@@ -1,7 +1,7 @@
 import {user} from "./Js_09_01";
 
-function icreaseAge (u:UserType) {
-    u.age ++;
+function icreaseAge(u: UserType) {
+    u.age++;
 }
 
 export type UserType = {
@@ -9,21 +9,52 @@ export type UserType = {
     age: number
 }
 
-test("big reference type test", ()=>{
+test("array test", () => {
 
-    let user: UserType = {
-        name: 'Elena',
-        age: 33
-    };
+    let users = [
+        {
+            name: 'Elena',
+            age: 33
+        },
+        {
+            name: 'Elena',
+            age: 33
+        },
+    ];
 
-    icreaseAge(user)
 
-    expect(user.age).toBe(34)
+    let admins = users
+    admins.push({name: 'Bandit', age: 11})
+
+    expect(users[2]).toEqual({name: 'Bandit', age: 11})
+    expect(users[2].name).toBe('Bandit')
+    expect(users[2].age).toBe(11)
+    expect(users[0].name).toBe('Elena')
 
     const supermen = user
     supermen.age = 1000;
 
-    expect(user.age).toBe(1000)
+    expect(users[1].age).toBe(33)
 
 })
+
+
+//-----------------------------------------------------------------------------------------------
+// test("big reference type test", ()=>{
+//
+//     let user: UserType = {
+//         name: 'Elena',
+//         age: 33
+//     };
+//
+//     icreaseAge(user)
+//
+//     expect(user.age).toBe(34)
+//
+//     const supermen = user
+//     supermen.age = 1000;
+//
+//     expect(user.age).toBe(1000)
+//
+// })
 
