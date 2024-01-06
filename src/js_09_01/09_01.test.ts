900046e3f218327a4e56162b686ce4532bbdb492
ElenaKuklina <cuklina.lena@gmail.com>
2024-01-06 19:07:30 +0300

    Lesson_09: writing "value type test"

--- src/js_09_01/09_01.test.ts
+++ src/js_09_01/09_01.test.ts
@@ -9,7 +9,7 @@ export type UserType = {
     age: number
 }
 
-test("array test", () => {
+test("array reference test", () => {
 
     let users = [
         {
@@ -37,6 +37,59 @@ test("array test", () => {
     expect(users[1].age).toBe(33)
 
 })
+
+
+test('value type test', ()=> {
+    let userCount = 100;
+
+    let adminCount = userCount
+
+    adminCount++;
+
+    expect(adminCount).toBe(101)
+    expect(adminCount).toEqual(adminCount)
+
+})
+
+
+
+
+
+
+
+
+
+//---------------------------------------------------------------------------------------------
+/*test("array test", () => {
+
+    let users = [
+        {
+            name: 'Elena',
+            age: 33
+        },
+        {
+            name: 'Elena',
+            age: 33
+        },
+    ];
+
+
+    let admins = users
+    admins.push({name: 'Bandit', age: 11})
+
+    expect(users[2]).toEqual({name: 'Bandit', age: 11})
+    expect(users[2].name).toBe('Bandit')
+    expect(users[2].age).toBe(11)
+    expect(users[0].name).toBe('Elena')
+
+    const supermen = user
+    supermen.age = 1000;
+
+    expect(users[1].age).toBe(33)
+
+})*/
+
+
 
 
 //-----------------------------------------------------------------------------------------------
