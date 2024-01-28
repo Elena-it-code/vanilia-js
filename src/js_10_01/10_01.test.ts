2ebfdf29c50622da947c5bb4769ba5caeb536aea
ElenaKuklina <cuklina.lena@gmail.com>
2024-01-28 23:55:33 +0300

    Lesson_10: immutability, иммутабельность. test("change address"). nesting object in object

--- src/js_10_01/10_01.test.ts
+++ src/js_10_01/10_01.test.ts
@@ -1,21 +1,14 @@
-import {UserType} from "./Js_10_01";
+import {makeHairstyle, moveUser, UserType, UserWithLaptopType} from "./Js_10_01";
 
-function makeHairstyle(u: UserType, power: number) {
-    const copy = {
-        ...u,
-        hair: u.hair / power
-    }
-    //copy.hair = u.hair / power;
 
-    return copy
-}
 
 test("Test for surface copying of an object. Without deep nesting", () => {
     let user: UserType = {
         name: 'Elena',
         hair: 32,
         address: {
-            title: 'Saint-Petersburg'
+            city: 'Saint-Petersburg',
+            house: 12
         }
     };
 
@@ -24,5 +17,28 @@ test("Test for surface copying of an object. Without deep nesting", () => {
     expect(user.hair).toBe(32)
     expect(awesomeUser.hair).toBe(16)
     expect(awesomeUser.address).toBe(user.address)
+})
+
+test("change address", () => {
+    let user: UserWithLaptopType = {
+        name: 'Elena',
+        hair: 32,
+        address: {
+            city: 'Saint-Petersburg',
+            house: 12
+        },
+        laptop: {
+            title: 'ZenBook'
+        }
+    };
+
+    const movedUser = moveUser(user, 'Moscow')
+
+    //user = movedUser
+
+    expect(user).not.toBe(movedUser)
+    expect(user.address).not.toBe(movedUser.address)
+    expect(user.laptop).toBe(movedUser.laptop)
+    expect(movedUser.address.city).toBe('Moscow')
 })
 
--- src/js_10_01/Js_10_01.ts
+++ src/js_10_01/Js_10_01.ts
@@ -4,6 +4,44 @@ export type UserType = {
     name: string
     hair: number,
     address: {
-        title: string
+        city: string,
+        house: number
+    }
+}
+
+export type LaptopType = {
+    title: string
+}
+
+export type UserWithLaptopType = UserType & {
+    laptop: LaptopType
+}
+export  function makeHairstyle(u: UserType, power: number) {
+    const copy = {
+        ...u,
+        hair: u.hair / power
+    }
+    //copy.hair = u.hair / power;
+
+    return copy
+}
+
+export  function moveUser (u: UserWithLaptopType, city: string) {
+    // 1 из вариантов записи
+    /*const copy = {
+        ...u, address: {...u.address, city: city}
+    }
+
+   /!* copy.address = {
+        ...copy.address,
+        city: city
+    }*!/
+
+    return copy*/
+
+    // Либо такой вариант. Так будет правильнее:
+    return {...u,
+        address: {...u.address,
+            city: city}
     }
 }
